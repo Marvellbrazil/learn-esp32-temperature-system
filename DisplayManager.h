2948e7764a66cf682ba844078b8a85ab598e6a79
Marvellbrazil <marvellbrazil@gmail.com>
2026-06-03 11:22:17 +0700

    Initial Commit

---
+++ DisplayManager.h
@@ -0,0 +1,23 @@
+#ifndef DISPLAY_MANAGER_H
+#define DISPLAY_MANAGER_H
+
+#include <LiquidCrystal_I2C.h>
+
+class DisplayManager
+{
+private:
+    Liquid
+public:
+    DisplayManager(/* args */);
+    ~DisplayManager();
+};
+
+DisplayManager::DisplayManager(/* args */)
+{
+}
+
+DisplayManager::~DisplayManager()
+{
+}
+
+#endif
---
+++ README.md
@@ -0,0 +1,6 @@
+# learn-esp32-temperature-system
+#### Currently learning Embedded Systems and IoT
+#### Feel free to give advices, criticize, and sharing
+
+<code>circuit_image.png</code>
+<img src="circuit_image.png"/>
